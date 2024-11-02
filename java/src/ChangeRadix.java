2e0da86e77fab763d6fc8b966b22d861bc731bee
hanako0516 <sor.16y@gmail.com>
2024-11-02 14:59:20 +0900

    いろんな基数と整数型

---
+++ java/src/ChangeRadix.java
@@ -0,0 +1,12 @@
+public class ChangeRadix {
+    public static void main(String[] args) throws Exception {
+
+        //16進数の506
+        int int16 = 0x1fa;
+        System.out.println(int16);
+
+        //10進数の20
+        int int10 = 20;
+        System.out.println(int16+ int10);
+    }
+}
