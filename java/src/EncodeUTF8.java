d3daf7a00fea734e5061da5137f3550cc8238abc
hanako0516 <sor.16y@gmail.com>
2024-11-02 17:13:46 +0900

    基数変換問題のコード

--- java/src/EncodeUTF8.java
+++ java/src/EncodeUTF8.java
@@ -1,5 +1,19 @@
 public class EncodeUTF8 {
     public static void main(String[] args) {
-        
+        //「あ」のEndodeの16進数
+        int codePoint = 0x3042;
+
+        System.out.println(Integer.toHexString(codePoint));
+
+        encode(codePoint);
+    }
+    private static void encode(int codePoint){
+        int[] utf8Bytes = {224,128,128};
+        int tmp = codePoint;
+        for(int i = utf8Bytes.length -1 ; i>=0;i--){
+            utf8Bytes[i] =  utf8Bytes[i]+ (tmp%64);
+            tmp = tmp/64;
+        }
+        System.out.println(utf8Bytes[0]+","+utf8Bytes[1]+","+utf8Bytes[2]);
     }
 }
